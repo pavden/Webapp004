339a6bc438920c21e7a221e8e9bba695ac29507e
Pavel Denisenko <pavel.denisenko@gmail.com>
2019-02-26 21:27:27 +0200

    Initial commit for the project

---
+++ src/main/java/ua/training/controller/command/Registration.java
@@ -0,0 +1,10 @@
+package ua.training.controller.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Registration implements Command {
+    @Override
+    public String execute(HttpServletRequest request) {
+        return "/registration.jsp";
+    }
+}
